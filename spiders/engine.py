ce611bc8b67b93f17b4567c6dbf746c693db2d22
xbkaishui <xbkaishui@126.com>
2020-12-06 19:17:49 +0800

    add cache for stock

--- spiders/engine.py
+++ spiders/engine.py
@@ -4,13 +4,14 @@ from spiders.symbols import get_exchange_symbols
 from spiders.common.constant import Exchange
 import os
 from spiders.nasdaq import NasdaqSpider
+from spiders.util import is_cached
 
 class CrawlerEngine(object):
 
     """
      sync all stock to local path
     """
-    def sync_all_nasdaq(self, file_path, start, end):
+    def sync_all_nasdaq(self, file_path, start, end, cache = True):
         spider = NasdaqSpider()
         symbols = get_exchange_symbols(Exchange.NASDAQ)
         print("symbols size " + str(len(symbols)))
@@ -19,6 +20,9 @@ class CrawlerEngine(object):
             try:
                 code = symbol.symbol
                 stock_path = os.path.join(file_path, code+".csv")
+                if cache and is_cached(stock_path):
+                    print("file is cached ignore " + stock_path)
+                    continue
                 print("try to sync symbol " + code )
                 spider.store_kline_data(stock_path, code, start, end)
             except :
