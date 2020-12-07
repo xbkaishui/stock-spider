f8d5590f603fc6c532615b822e13ee7e569cb27b
xbkaishui <xbkaishui@126.com>
2020-12-07 15:59:13 +0800

    performance improve, add multi-threading

--- spiders/engine.py
+++ spiders/engine.py
@@ -5,32 +5,48 @@ from spiders.common.constant import Exchange
 import os
 from spiders.nasdaq import NasdaqSpider
 from spiders.util import is_cached
+from multiprocessing import Pool
 
 class CrawlerEngine(object):
+
+    def __init__(self):
+        self.spider = NasdaqSpider()
 
     """
      sync all stock to local path
     """
     def sync_all_nasdaq(self, file_path, start, end, cache = True):
-        spider = NasdaqSpider()
         symbols = get_exchange_symbols(Exchange.NASDAQ)
         print("symbols size " + str(len(symbols)))
         self.create_dir_if_needed(file_path)
+        cpu_count = os.cpu_count()
+        pool = Pool(cpu_count)
+        sync_params = []
         for symbol in symbols:
-            try:
-                code = symbol.symbol
-                stock_path = os.path.join(file_path, code+".csv")
-                if cache and is_cached(stock_path):
-                    print("file is cached ignore " + stock_path)
-                    continue
-                print("try to sync symbol " + code )
-                spider.store_kline_data(stock_path, code, start, end)
-            except :
-                print("fetch symbol error " + symbol.symbol + " ignore")
+            param = {"symbol":symbol, "file_path":file_path, "start":start, "end":end, "cache":cache}
+            sync_params.append(param)
+        pool.map(self.sync_single_symbol, sync_params)
 
     def create_dir_if_needed(self, file_path):
         if not os.path.exists(file_path):
             os.mkdir(file_path)
+
+    def sync_single_symbol(self, param):
+        symbol = param['symbol']
+        file_path = param['file_path']
+        start  = param['start']
+        end = param['end']
+        cache = param['cache']
+        try:
+            code = symbol.symbol
+            stock_path = os.path.join(file_path, code+".csv")
+            if cache and is_cached(stock_path):
+                print("file is cached ignore " + stock_path)
+                return
+            print("try to sync symbol " + code )
+            self.spider.store_kline_data(stock_path, code, start, end)
+        except :
+            print("fetch symbol error " + symbol.symbol + " ignore")
 
 
 
